ec161b6c09ed4d6e92e81d47fbf551ebc6d8deda
shewit <se.shewit.ghiwot@gmail.com>
2021-02-21 10:12:41 -0800

    Displayed the users information on the profile page

---
+++ assets/js/profile.js
@@ -0,0 +1,5 @@
+
+// document.getElementById("userNameDisplay").innerHTML = (JSON.parse(localStorage.getItem('user'))).uname
+
+// document.getElementById("emailDisplay").innerHTML = (JSON.parse(localStorage.getItem('user'))).email
+
